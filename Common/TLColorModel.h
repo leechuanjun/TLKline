6e95c714599d06e419db8ad10eb617329f545609
lichuanjun <lichuanjun@192.168.1.111>
2015-09-26 16:38:23 +0800

    新增代码

---
+++ Common/TLColorModel.h
@@ -0,0 +1,19 @@
+//
+//  TLColorModel.h
+//  TLKline
+//
+//  Created by lichuanjun on 15/9/26.
+//  Copyright © 2015年 lichuanjun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface TLColorModel : NSObject
+
+@property (nonatomic,assign) int R;
+@property (nonatomic,assign) int G;
+@property (nonatomic,assign) int B;
+@property (nonatomic,assign) CGFloat alpha;
+
+@end
---
+++ Common/TLColorModel.m
@@ -0,0 +1,13 @@
+//
+//  TLColorModel.m
+//  TLKline
+//
+//  Created by lichuanjun on 15/9/26.
+//  Copyright © 2015年 lichuanjun. All rights reserved.
+//
+
+#import "TLColorModel.h"
+
+@implementation TLColorModel
+
+@end
